16731fc4c253fc56edf38890de1848ce500d396b
hesterbronnvik <Pitohui1@gmail.com>
2021-10-12 21:11:11 +0200

    Initial code

--- following_S2.R
+++ following_S2.R
@@ -19,7 +19,7 @@ library(bcpa)
 
 
 # Create a login object for a user account at movebank.org
-loginStored <- movebankLogin(username="hebronnvik", password="Sapereaude!1")
+loginStored <- movebankLogin(username=)
 
 # Get overview information about a Movebank study. Be sure to check the citation and license terms if not using your own data.
 getMovebankStudy(study="LifeTrack Golden Eagle Alps", login=loginStored) # see study-level info
@@ -32,44 +32,58 @@ head(eagle_move)
 plot(eagle_move[, 1:2], pch = 20, xlab = "", ylab = "", las = 1, type = "l",
      lwd = 0.5, asp = 1)
 
+eagle_move_thin <- eagle_move[timestamps(eagle_move) >= as.POSIXct("2021-03-20 12:10:14") &
+                                timestamps(eagle_move) < as.POSIXct("2021-03-20 12:25:14")]
+data_sp <- as(eagle_move_thin, "data.frame") # store the data, then create a spatial object for plotting
+coordinates(data_sp) <- ~ location_long + location_lat # set coordinates
+proj4string(data_sp) <- wgs # set projection
+mapView(data_sp, cex = 3, color = rainbow) # plot on a map
+
+eagle_df <- as(eagle_move_thin, "data.frame")
+
+# Data cleaning
+
+# Delete observations missing lat or long or a timestamp.
+ind<-complete.cases(eagle_df[,c("location_lat", "location_long", "timestamp")])
+eagle_df<-eagle_df[ind==TRUE,]
+
+# Check for duplicated observations (ones with same lat, long, timestamp,
+# and individual identifier). 
+ind2<-eagle_df %>% select(timestamp, location_long, location_lat, local_identifier) %>%
+  duplicated
+sum(ind2) # no duplicates
+eagle_df<-eagle_df[ind2!=TRUE,]
+
+rm(ind)
+rm(ind2)
+
+
+# Make timestamp a date/time variable
+eagle_df$timestamp<-as.POSIXct(eagle_df$timestamp, format="%Y-%m-%d %H:%M:%OS", tz="UTC")
 
 eagle_df$sl <- c(NA, with(eagle_df, sqrt((head(location_long, -1) - tail(location_long, -1))^2 +
-                               (head(location_lat, -1) - tail(location_lat, -1))^2)))
+                                           (head(location_lat, -1) - tail(location_lat, -1))^2)))
 eagle_df$nsd <- with(eagle_df, sqrt((location_long - eagle_df$location_long[1])^2 + (location_lat - eagle_df$location_lat[1])^2))
 dat <- prepData(eagle_df[, c("location_long", "location_lat", "sl", "nsd")],coordNames = c("location_long", "location_lat"), type = "UTM")
 head(dat)
-hist(dat$step, xlim = c(0,0.001), breaks = 50000)
-hist(dat$angle, breaks = 50000)
-hist(dat$nsd, breaks = 50000)
+hist(dat$step, breaks = 50)
+hist(dat$angle, breaks = 50)
+hist(dat$nsd, breaks = 50)
 
 table(diff(eagle_df$timestamp))
 wawotest(dat$step)
 wawotest(dat$nsd)
 
 ## BCPA
-eagle_df_test <- eagle_df
-eagle_df <- eagle_df[1:10000,]
 eagle_df$timestamp2 <- 1:nrow(eagle_df)
 path_char <- bcpa::MakeTrack(eagle_df$location_long, eagle_df$location_lat, eagle_df$timestamp2)
 path_char <- bcpa::GetVT(path_char)
 path_char$nsd <- eagle_df$nsd[-(1:2)]
 # run the bcpa
-ws <- WindowSweep(path_char, "Theta", windowsize = 300, progress = T)
-plot(ws, type = "flat", clusterwidth = 24 * 7,
+ws <- WindowSweep(path_char, "Theta", windowsize = 10, progress = T)
+plot(ws, type = "flat", clusterwidth = 30,
      xlab = "time", las = 1, ylab = "turning angle")
 eagle_bc <- data.frame(Time=eagle_df$timestamp, X=eagle_df$location_long, Y=eagle_df$location_lat)
-PathPlot(eagle_bc,ws, type = "flat",clusterwidth = 24 * 7,
-         plotlegend = TRUE, tauwhere = "topleft",
+PathPlot(eagle_bc,ws, type = "flat",clusterwidth = 30,
+         plotlegend = TRUE, tauwhere = "right",
          n.legend = 4, ncol.legend = 2, bty.legend = TRUE)
-
-
-
-
-
-
-
-
-
-
-
-
